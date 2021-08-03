36bd88322e0a46dc2a60d7b9bf9a8897869fb418
Rafika <RTuremis@hotmail.co.uk>
2021-08-03 13:54:21 +0100

    Refactoring files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import Artists from './components/Artists';
-import Songs from './components/Songs';
+import React, { Component } from 'react';
+import { Artists, Songs } from './components';
 
 const App = () => {
-    return (
-        <>
-            <Artists name="Ariana Grande" musicgenre="pop" intro="hfewjr jkebiwr "/>
-            <Songs album={["into you", "seven rings"]}/>
-            <Artists name="Rafika" musicgenre="pop" intro="Rafika is a good artist "/>
-            <Songs album={["talking crazy", "hello world", "tada"]}/>
-        </>
-    )
+  return (
+    <>
+      <Artists name="Ariana Grande" musicgenre="pop" intro="hfewjr jkebiwr " />
+      <Songs album={['into you', 'seven rings']} />
+      <Artists name="Rafika" musicgenre="pop" intro="Rafika is a good artist " />
+      <Songs album={['talking crazy', 'hello world', 'tada']} />
+    </>
+  );
 };
 
 export default App;
